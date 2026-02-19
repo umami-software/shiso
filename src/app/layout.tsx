b50dfba0d929d8e83c067bab11a78628d631509c
Mike Cao <mike@mikecao.com>
2026-02-18 23:50:53 -0800

    Updated packages.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,9 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/inter/800.css';
 import '@fontsource/jetbrains-mono/400.css';
-import '@umami/react-zen/styles.css';
 import 'highlight.js/styles/github-dark.css';
+import './tailwind.css';
+import '@umami/react-zen/styles.css';
 import './global.css';
 import { App } from '@/app/App';
 
